d9bfabad537430639be87656e8f0e5b3b59a91b2
Andrey Skyba <andrey.skyba.2015@gmail.com>
2023-10-14 20:20:19 +0300

    FixMenu

--- Assets/Scripts/MenuButtons.cs
+++ Assets/Scripts/MenuButtons.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManager;
+using UnityEngine.SceneManagement;
 
 
 public class MenuButtons : MonoBehaviour
 {
-   public void PlayGame(){
-    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+   public void PlayGame()
+   {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
    }
 
-   public void Exit(){
-    Application.Quit();
+   public void Exit()
+   {
+        Application.Quit();
    }
 }
